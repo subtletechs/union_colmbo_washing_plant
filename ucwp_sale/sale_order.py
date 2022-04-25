8f5b761f06eb7ae12ba8fae2293794cc88f275db
Milan <milanb@subtletechs.com>
2022-04-25 17:27:14 +0530

    [UPDATED]- Sale order line updated(partially)

--- ucwp_sale/sale_order.py
+++ ucwp_sale/sale_order.py
@@ -12,3 +12,24 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     garment_type = fields.Selection([('bulk', 'Bulk'), ('sample', 'Sample')], string='Bulk/Sample', required=True)
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    buyer = fields.Many2one(comodel_name="res.partner", string="Buyer")
+    style_no = fields.Char(string="Style No")
+    fabric_type = fields.Many2one(comodel_name="fabric.type", string="Fabric Type")
+    wash_type = fields.Many2one(comodel_name="wash.type", string="Wash Type")
+    garment_type = fields.Many2one(comodel_name="garment.type", string="Garment Type")
+    garment_select = fields.Selection([('bulk', 'Bulk'), ('sample', 'Sample')], string='Bulk/Sample')
+
+    @api.onchange('product_id')
+    def _onchange_details(self):
+        self.buyer = self.product_id.buyer
+        self.style_no = self.product_id.style_no
+        self.fabric_type = self.product_id.fabric_type
+        self.wash_type = self.product_id.wash_type
+        self.garment_type = self.product_id.garment_type
+        self.garment_select = self.product_id.garment_select
+
