ffc8f7f45773b84819276fbe0cca076db46e9973
Sachitha Perera <sperera@nurosoft.id>
2022-05-30 13:05:40 +0530

    [FIXED] - UC-28 - Style Expire and Date values not saving , [FIXED] - Runtime error - missing syntax

--- ucwp_stock/res_config_settings.py
+++ ucwp_stock/res_config_settings.py
@@ -5,8 +5,10 @@ from datetime import date, timedelta
 class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
-    style_expire = fields.Boolean(string="Style Expire Timer")
-    days = fields.Integer(string="Days")
+    style_expire = fields.Boolean(string="Style Expire Timer",
+                                  config_parameter='union_colmbo_washing_plant.style_expire')
+    days = fields.Integer(string="Days",
+                          config_parameter='union_colmbo_washing_plant.days')
 
     def action_style_expire(self):
         set_days = self.days
