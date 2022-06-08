69f4e83ca7b55f9ad943d05fa11f794164f5cf0e
Milan <milanb@subtletechs.com>
2022-06-09 01:45:53 +0530

    [UPDATED]- UC-45 Set picking type for Chemicals

--- ucwp_purchase/purchase.py
+++ ucwp_purchase/purchase.py
@@ -5,3 +5,12 @@ class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
     chemical_purchase = fields.Boolean(string="Chemical Purchase", default=False)
+
+    @api.onchange('chemical_purchase')
+    def update_picking_type(self):
+        """Set Chemical Receipt as Stock Operation Type """
+        if self.chemical_purchase:
+            picking_type = self.env['stock.picking.type'].search(
+                [('chemical_receipt', '=', True)])
+            if picking_type:
+                self.picking_type_id = picking_type.id
