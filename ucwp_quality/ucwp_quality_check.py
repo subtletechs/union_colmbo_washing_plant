d7b081447b9d456ccf662ef5815c31085e59c3a3
Milan <milanb@subtletechs.com>
2022-06-29 14:24:07 +0530

    [ADDED]- Add Quality check line info model and view, pass/fail information move to info model, [UPDATED]- Quality check line info total info line quantities validated

--- ucwp_quality/ucwp_quality_check.py
+++ ucwp_quality/ucwp_quality_check.py
@@ -36,8 +36,8 @@ class UCWPQualityCheck(models.Model):
                 product_list.append(product.id)
                 product_names.append(product.name)
         if len(product_list) > 0:
-            products = ' '.join([str(name)+',' for name in product_names])
-            error = "Product"+" - " + products + " Inspected Quantity and Process Quantity Not matched "
+            products = ' '.join([str(name) + ',' for name in product_names])
+            error = "Product" + " - " + products + " Inspected Quantity and Process Quantity Not matched "
             raise ValidationError(error)
         else:
             self.write({'state': 'lock'})
@@ -57,25 +57,102 @@ class QualityCheckLines(models.Model):
 
     product = fields.Many2one(comodel_name="product.product", string="Product")
     lot_no = fields.Many2one(comodel_name="stock.production.lot", string="Lot No", required=True)
+    ucwp_quality_check_id = fields.Many2one(comodel_name="ucwp.quality.check", string="Quality Check")
+    # Functional Testing 15.
+    inspected_qty = fields.Float(string="Inspected Quantity")
+
+    quality_point = fields.Selection([('before_wash', 'Before Wash'), ('after_wash', 'After Wash')],
+                                     string="Quality Point")
+
+    # UCWP|IMP|-00048 Quality check process - Re-engineering
+    quality_check_line_info = fields.One2many(comodel_name="quality.check.line.info",
+                                              inverse_name="quality_check_line_id", string="Quality Check Info")
+    rest_qty = fields.Float(string="Rest Inspected qty", compute="_set_rest_inspected_qty")
+
+    # Set initial value to rest qty
+    @api.depends('quality_check_line_info.quantity')
+    def _set_rest_inspected_qty(self):
+        for line in self:
+            quantity_done = 0
+            for quality_check_info_record in line.quality_check_line_info:
+                quantity_done += quality_check_info_record.quantity
+            line.rest_qty = quantity_done
+            if quantity_done > self.inspected_qty:
+                raise ValidationError(_("Total of Pass/Fail quantities cannot exceed inspected quantity"))
+
+    @api.onchange('product', 'lot_no')
+    def update_domain(self):
+        """Set demain to filter product and Lot No"""
+        product_ids = self.ucwp_quality_check_id.grn.move_ids_without_package.product_id.ids
+        split_lines = self.ucwp_quality_check_id.grn.move_ids_without_package.move_line_nosuggest_ids
+        lot_name_list = []
+        for split_line in split_lines:
+            if split_line.product_id.id == self.product.id:
+                lot_name_list.append(split_line.barcode)
+        lot_ids = self.env['stock.production.lot'].search([('name', 'in', lot_name_list)])
+        if lot_ids:
+            return {
+                'domain': {'lot_no': [('id', 'in', lot_ids.ids)]}
+            }
+        if product_ids:
+            return {
+                'domain': {'product': [('id', 'in', product_ids)]}
+            }
+
+
+class QualityCheckLineInfo(models.Model):
+    _name = "quality.check.line.info"
+    _description = "Quality Check line Information"
+
+    # UCWP|IMP|-00048 Quality check process - Re-engineering
     pass_fail = fields.Selection([('pass', 'Pass'), ('fail', 'Fail')], string="Pass/Fail", required=True)
-    quantity = fields.Float(string="Pass/Fail Quantity", required=True)
+    quantity = fields.Float(string="Quantity", required=True)
     defects = fields.Many2one(comodel_name="defects", string="Defects")
     image = fields.Binary(string="Image")
-    ucwp_quality_check_id = fields.Many2one(comodel_name="ucwp.quality.check", string="Quality Check")
     comment = fields.Char(string="Comment")
     state = fields.Selection([('processed', 'Process'), ('returned', 'Returned'),
                               ('rewashed', 'Rewash')], string="State", readonly=True)
+    quality_check_line_id = fields.Many2one(comodel_name="quality.check.lines", string="Quality Check Line ID")
     quality_point = fields.Selection([('before_wash', 'Before Wash'), ('after_wash', 'After Wash')],
                                      string="Quality Point")
-    # Functional Testing 15.
-    inspected_qty = fields.Float(string="Inspected Quantity")
 
     display_process_button = fields.Boolean(compute="_display_button")
     display_return_button = fields.Boolean(compute="_display_button")
     display_rewash_button = fields.Boolean(compute="_display_button")
+
     # display_dispose_button = fields.Boolean(compute="_display_button")
 
+    @api.depends('quality_point', 'pass_fail')
+    def _display_button(self):
+        # Process Button visibility
+        for record in self:
+            if record.quality_point == 'before_wash' and record.pass_fail == 'fail':
+                record.display_process_button = True
+            elif record.quality_point == 'after_wash' and record.pass_fail == 'pass':
+                record.display_process_button = True
+            else:
+                record.display_process_button = False
+
+            # Dispose Button visibility
+            # if record.pass_fail == 'fail':
+            #     record.display_dispose_button = True
+            # else:
+            #     record.display_dispose_button = False
+
+            # Rewash Button visibility
+            if record.quality_point == 'after_wash' and record.pass_fail == 'fail':
+                record.display_rewash_button = True
+            else:
+                record.display_rewash_button = False
+
+            # Return Button visibility
+            if record.quality_point == 'before_wash' and record.pass_fail == 'fail':
+                record.display_return_button = True
+            else:
+                record.display_return_button = False
+
     def process_garment(self):
+        """Process garment"""
         if self.quality_point == 'before_wash' and self.pass_fail == 'fail':
             self.write({'state': 'processed'})
         if self.quality_point == 'after_wash' and self.pass_fail == 'pass':
@@ -138,52 +215,4 @@ class QualityCheckLines(models.Model):
     #             'default_picking_type_id': internal_transfer_operation.id,
     #         },
     #     }
-
-    @api.depends('quality_point', 'pass_fail')
-    def _display_button(self):
-        # Process Button visibility
-        for record in self:
-            if record.quality_point == 'before_wash' and record.pass_fail == 'fail':
-                record.display_process_button = True
-            elif record.quality_point == 'after_wash' and record.pass_fail == 'pass':
-                record.display_process_button = True
-            else:
-                record.display_process_button = False
-
-            # Dispose Button visibility
-            # if record.pass_fail == 'fail':
-            #     record.display_dispose_button = True
-            # else:
-            #     record.display_dispose_button = False
-
-            # Rewash Button visibility
-            if record.quality_point == 'after_wash' and record.pass_fail == 'fail':
-                record.display_rewash_button = True
-            else:
-                record.display_rewash_button = False
-
-            # Return Button visibility
-            if record.quality_point == 'before_wash' and record.pass_fail == 'fail':
-                record.display_return_button = True
-            else:
-                record.display_return_button = False
-
-    @api.onchange('product', 'lot_no')
-    def update_domain(self):
-        """Set demain to filter product and Lot No"""
-        product_ids = self.ucwp_quality_check_id.grn.move_ids_without_package.product_id.ids
-        split_lines = self.ucwp_quality_check_id.grn.move_ids_without_package.move_line_nosuggest_ids
-        lot_name_list = []
-        for split_line in split_lines:
-            if split_line.product_id.id == self.product.id:
-                lot_name_list.append(split_line.barcode)
-        lot_ids = self.env['stock.production.lot'].search([('name', 'in', lot_name_list)])
-        if lot_ids:
-            return {
-                'domain': {'lot_no': [('id', 'in', lot_ids.ids)]}
-            }
-        if product_ids:
-            return {
-                'domain': {'product': [('id', 'in', product_ids)]}
-            }
 
