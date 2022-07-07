bdaf983e1768399b0f470b4a3989861d385c4096
Sachitha Perera <sperera@nurosoft.id>
2022-07-07 15:56:31 +0530

    [FIXED] -  Customer profile payment methods

--- ucwp_sale/res_partner.py
+++ ucwp_sale/res_partner.py
@@ -9,13 +9,13 @@ class Partner(models.Model):
     credit_limit_available = fields.Boolean(string="Credit Limit Available", default=False)
     credit_limit = fields.Monetary(string="Credit Limit")
     currency_id = fields.Many2one(default=lambda self: self.env.company.currency_id, string="Currency")
-    available_credit_limit = fields.Monetary(currency_field='currency_id', string="Available Credit Limit",
-                                             compute='compute_available_credit_limit')
-    total_pending_payments = fields.Monetary(currency_field='currency_id', string="Total Pending Payments",
-                                             compute='compute_total_pending')
-
-    def compute_total_pending(self):
-        self.total_pending_payments = self.total_due
-
-    def compute_available_credit_limit(self):
-        self.available_credit_limit = self.credit_limit - self.total_due
+    # available_credit_limit = fields.Monetary(currency_field='currency_id', string="Available Credit Limit",
+    #                                          compute='compute_available_credit_limit')
+    # total_pending_payments = fields.Monetary(currency_field='currency_id', string="Total Pending Payments",
+    #                                          compute='compute_total_pending')
+    #
+    # def compute_total_pending(self):
+    #     self.total_pending_payments = self.total_due
+    #
+    # def compute_available_credit_limit(self):
+    #     self.available_credit_limit = self.credit_limit - self.total_due
