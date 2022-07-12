0333c5668c45afb1b2ac303755bfb765dd6deb75
Sachitha Perera <sperera@nurosoft.id>
2022-07-12 07:58:29 +0530

    [FIXED] -  Updated Credit Limit back

--- ucwp_sale/res_partner.py
+++ ucwp_sale/res_partner.py
@@ -9,36 +9,80 @@ class Partner(models.Model):
     credit_limit_available = fields.Boolean(string="Credit Limit Available", default=False)
     credit_limit = fields.Monetary(string="Credit Limit")
     currency_id = fields.Many2one(default=lambda self: self.env.company.currency_id, string="Currency")
-    available_credit_limit = fields.Monetary(currency_field='currency_id', string="Available Credit Limit")
-    total_pending_payments = fields.Monetary(currency_field='currency_id', string="Total Pending Payments")
-    # total_due = fields.Monetary(compute='_compute_total_due')
+    available_credit_limit = fields.Monetary(currency_field='currency_id', string="Available Credit Limit",
+                                             compute='compute_available_credit_limit')
+    total_pending_payments = fields.Monetary(currency_field='currency_id', string="Total Pending Payments",
+                                             compute='compute_total_pending')
 
-    # # @api.depends_context('company', 'allowed_company_ids')
-    # def _compute_total_due(self):
-    #     """
-    #     Compute the fields 'total_due'
-    #     """
-    #     today = fields.Date.context_today(self)
-    #     for record in self:
-    #         total_due = 0
-    #         total_overdue = 0
-    #         for aml in record.unreconciled_aml_ids:
-    #             if aml.company_id == self.env.company and not aml.blocked:
-    #                 amount = aml.amount_residual
-    #                 total_due += amount
-    #                 is_overdue = today > aml.date_maturity if aml.date_maturity else today > aml.date
-    #                 if is_overdue:
-    #                     total_overdue += amount
-    #         record.total_due = total_due
+    """----------------------Related to Credit Limit calculation from Account Package----------------------------"""
+    unpaid_invoices = fields.One2many('account.move', compute='_compute_unpaid_invoices')
+    unreconciled_aml_ids = fields.One2many('account.move.line', compute='_compute_unreconciled_aml_ids')
+    total_due = fields.Monetary(compute='_compute_total_due')
 
-    # def compute_total_pending(self):
-    #     for record in self:
-    #         if record.credit_limit_available:
-    #             record.total_pending_payments = record.total_due
-    #
-    # def compute_available_credit_limit(self):
-    #     for record in self:
-    #         if record.credit_limit_available:
-    #             record.available_credit_limit = record.credit_limit - record.total_due
+    def _compute_unpaid_invoices(self):
+        """Get unpaid customer invoices"""
+        for record in self:
+            record.unpaid_invoices = self.env['account.move'].search([
+                ('company_id', '=', self.env.company.id),
+                ('commercial_partner_id', '=', record.id),
+                ('state', '=', 'posted'),
+                ('payment_state', 'in', ('not_paid', 'partial')),
+                ('move_type', 'in', self.env['account.move'].get_sale_types())
+            ]).filtered(lambda inv: not any(inv.line_ids.mapped('blocked')))
+
+    @api.depends('invoice_ids')
+    @api.depends_context('company', 'allowed_company_ids')
+    def _compute_unreconciled_aml_ids(self):
+        values = {
+            read['partner_id'][0]: read['line_ids']
+            for read in self.env['account.move.line'].read_group(
+                domain=self._get_unreconciled_aml_domain(),
+                fields=['line_ids:array_agg(id)'],
+                groupby=['partner_id']
+            )
+        }
+        for partner in self:
+            partner.unreconciled_aml_ids = values.get(partner.id, False)
+
+    def _get_unreconciled_aml_domain(self):
+        return [
+            ('reconciled', '=', False),
+            ('account_id.deprecated', '=', False),
+            ('account_id.internal_type', '=', 'receivable'),
+            ('move_id.state', '=', 'posted'),
+            ('partner_id', 'in', self.ids),
+            ('company_id', '=', self.env.company.id),
+        ]
+
+    @api.depends_context('company', 'allowed_company_ids')
+    def _compute_total_due(self):
+        """
+        Compute the fields 'total_due'
+        """
+        today = fields.Date.context_today(self)
+        for record in self:
+            total_due = 0
+            total_overdue = 0
+            for aml in record.unreconciled_aml_ids:
+                if aml.company_id == self.env.company and not aml.blocked:
+                    amount = aml.amount_residual
+                    total_due += amount
+                    is_overdue = today > aml.date_maturity if aml.date_maturity else today > aml.date
+                    if is_overdue:
+                        total_overdue += amount
+            record.total_due = total_due
+
+    """------------------End - From Core Account Followups----------------------"""
+
+
+    def compute_total_pending(self):
+        for record in self:
+            if record.credit_limit_available:
+                record.total_pending_payments = record.total_due
+
+    def compute_available_credit_limit(self):
+        for record in self:
+            if record.credit_limit_available:
+                record.available_credit_limit = record.credit_limit - record.total_due
 
 
