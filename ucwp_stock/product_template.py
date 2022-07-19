2aed690b3af949e3dea037788044ab79659bcb46
Milan <milanb@subtletechs.com>
2022-07-19 12:56:04 +0530

    [UPDATED] - MSDS II rename to MSDS Information, Remove MSDS I tab from product template and variant, Add MSDS Information to product variant

--- ucwp_stock/product_template.py
+++ ucwp_stock/product_template.py
@@ -38,15 +38,11 @@ class ProductTemplate(models.Model):
     garment_select = fields.Selection([('bulk', 'Bulk'), ('sample', 'Sample')], string='Bulk/Sample')
     samples = fields.Many2one(comodel_name="garment.sample", string="Sample Type")
 
-    # UC-21 chemical MSDS descriptions
-    msds_in_english = fields.Text(string='In English')
-    msds_in_sinhala = fields.Text(string='In Sinhala')
-
     # UCWP|IMP|-00050 MSDS II tab
     other_name = fields.Many2one(comodel_name="other.name", string="Other Name")
     manufacturer_formulator = fields.Many2one(comodel_name="formulator", string="Manufacturer / Formulator(SDS)")
     chemical_formulator_type = fields.Many2one(comodel_name="chemical.formulator.type",
-                                               string="Chemical Formulator type")
+                                               string="Chemical Formulator Type")
     local_agent = fields.Many2one(comodel_name="res.partner", string="Local Agent")
     environment = fields.Selection([('low', 'Low'), ('medium', 'Medium'), ('high', 'High')], string="Environment")
     worker_health = fields.Selection([('low', 'Low'), ('medium', 'Medium'), ('high', 'High')], string="Worker Health")
@@ -59,7 +55,7 @@ class ProductTemplate(models.Model):
     version = fields.Float(string="Version")
     revision_date = fields.Date(string="Revision Date")
     explosive = fields.Boolean(string="Explosive")
-    health_hazard = fields.Boolean(string="health Hazard")
+    health_hazard = fields.Boolean(string="Health Hazard")
     flammable = fields.Boolean(string="Flammable")
     environmental_hazard = fields.Boolean(string="Environmental Hazard")
     toxic = fields.Boolean(string="Toxic")
@@ -69,10 +65,10 @@ class ProductTemplate(models.Model):
     warning_irritant = fields.Boolean(string="Warning/ Irritant")
     respirator = fields.Boolean(string="Respirator")
     gloves = fields.Boolean(string="Gloves")
-    footwear = fields.Boolean(string="Footwear")
+    footwear = fields.Boolean(string="Foot Wear")
     eye_wear = fields.Boolean(string="Eye Wear")
     apron = fields.Boolean(string="Apron")
-    dust_mask = fields.Boolean(string="Dust mask")
+    dust_mask = fields.Boolean(string="Dusk Mask")
     overalls = fields.Boolean(string="Overalls")
     face_shield = fields.Boolean(string="Face Shield")
     safety_helmet = fields.Boolean(string="Safety Helmet")
@@ -95,17 +91,17 @@ class ProductTemplate(models.Model):
         [('not_available', 'N/A'), ('contact0', '0'), ('contact1', '1'), ('contact2', '2'),
          ('contact3', '3'), ('contact4', '4'), ('contact5', '5')], string="Contact")
     severity = fields.Selection(
-        [('0', '0'), ('1', '1'), ('2', '2'),
+        [('1', '1'), ('2', '2'),
          ('3', '3'), ('4', '4'), ('5', '5')], string="Severity")
     likelihood = fields.Selection(
-        [('0', '0'), ('1', '1'), ('2', '2'),
+        [('1', '1'), ('2', '2'),
          ('3', '3'), ('4', '4'), ('5', '5')], string="Likelihood")
     risk_rating = fields.Integer(string="Risk Rating")
     storage_class = fields.Many2one(comodel_name="storage.class", string="Storage Class")
-    water = fields.Float(string="Water")
-    foom = fields.Float(string="Foom")
-    dry_powder = fields.Float(string="Dry Powder")
-    carbon_dioxide = fields.Float(string="Carbon Dioxide")
+    water = fields.Boolean(string="Water")
+    foom = fields.Boolean(string="Foom")
+    dry_powder = fields.Boolean(string="Dry Powder")
+    carbon_dioxide = fields.Boolean(string="Carbon Dioxide")
     inhalation_english = fields.Text(string="Inhalation (English)")
     inhalation_sinhala = fields.Text(string="Inhalation (Sinhala)")
     eye_contact_english = fields.Text(string="Eye Contact (English)")
@@ -225,9 +221,108 @@ class ProductProduct(models.Model):
     samples = fields.Many2one(comodel_name="garment.sample", string="Samples", related='product_tmpl_id.samples',
                               store=True)
 
-    # [UC-21]- chemical MSDS descriptions
-    msds_in_english = fields.Text(string='In English', related='product_tmpl_id.msds_in_english', store=True)
-    msds_in_sinhala = fields.Text(string='In Sinhala', related='product_tmpl_id.msds_in_sinhala', store=True)
+    # UCWP|IMP|-00050 MSDS II tab
+    other_name = fields.Many2one(comodel_name="other.name", string="Other Name", related="product_tmpl_id.other_name")
+    manufacturer_formulator = fields.Many2one(comodel_name="formulator", string="Manufacturer / Formulator(SDS)",
+                                              related="product_tmpl_id.manufacturer_formulator")
+    chemical_formulator_type = fields.Many2one(comodel_name="chemical.formulator.type",
+                                               string="Chemical Formulator Type",
+                                               related="product_tmpl_id.chemical_formulator_type")
+    local_agent = fields.Many2one(comodel_name="res.partner", string="Local Agent",
+                                  related="product_tmpl_id.local_agent")
+    environment = fields.Selection([('low', 'Low'), ('medium', 'Medium'), ('high', 'High')], string="Environment",
+                                   related="product_tmpl_id.environment")
+    worker_health = fields.Selection([('low', 'Low'), ('medium', 'Medium'), ('high', 'High')], string="Worker Health",
+                                     related="product_tmpl_id.worker_health")
+    location = fields.Many2one(comodel_name="location", string="Location", related="product_tmpl_id.location")
+    category = fields.Many2one(comodel_name="chemical.category", string="Category",
+                               related="product_tmpl_id.category")
+    chemical_type = fields.Many2one(comodel_name="chemical.type", string="Chemical Type",
+                                    related="product_tmpl_id.chemical_type")
+    used_for = fields.Many2one(comodel_name="chemical.used.for", string="Used for",
+                               related="product_tmpl_id.used_for")
+    available = fields.Boolean(string="Available", related="product_tmpl_id.available")
+    issue_date = fields.Date(string="Issue Date", related="product_tmpl_id.issue_date")
+    version = fields.Float(string="Version", related="product_tmpl_id.version")
+    revision_date = fields.Date(string="Revision Date", related="product_tmpl_id.revision_date")
+    explosive = fields.Boolean(string="Explosive", related="product_tmpl_id.explosive")
+    health_hazard = fields.Boolean(string="Health Hazard", related="product_tmpl_id.health_hazard")
+    flammable = fields.Boolean(string="Flammable", related="product_tmpl_id.flammable")
+    environmental_hazard = fields.Boolean(string="Environmental Hazard", related="product_tmpl_id.environmental_hazard")
+    toxic = fields.Boolean(string="Toxic", related="product_tmpl_id.toxic")
+    corrosive = fields.Boolean(string="Corrosive", related="product_tmpl_id.corrosive")
+    oxidizing = fields.Boolean(string="Oxidizing", related="product_tmpl_id.oxidizing")
+    compressed_gas = fields.Boolean(string="Compressed Gas", related="product_tmpl_id.compressed_gas")
+    warning_irritant = fields.Boolean(string="Warning/ Irritant", related="product_tmpl_id.warning_irritant")
+    respirator = fields.Boolean(string="Respirator", related="product_tmpl_id.respirator")
+    gloves = fields.Boolean(string="Gloves", related="product_tmpl_id.gloves")
+    footwear = fields.Boolean(string="Foot Wear", related="product_tmpl_id.footwear")
+    eye_wear = fields.Boolean(string="Eye Wear", related="product_tmpl_id.eye_wear")
+    apron = fields.Boolean(string="Apron", related="product_tmpl_id.apron")
+    dust_mask = fields.Boolean(string="Dusk Mask", related="product_tmpl_id.dust_mask")
+    overalls = fields.Boolean(string="Overalls", related="product_tmpl_id.overalls")
+    face_shield = fields.Boolean(string="Face Shield", related="product_tmpl_id.face_shield")
+    safety_helmet = fields.Boolean(string="Safety Helmet", related="product_tmpl_id.safety_helmet")
+    measures_for_cleaning_english = fields.Text(string="Measures for Cleaning (English)",
+                                                related="product_tmpl_id.measures_for_cleaning_english")
+    measures_for_cleaning_sinhala = fields.Text(string="Measures for Cleaning (Sinhala)",
+                                                related="product_tmpl_id.measures_for_cleaning_sinhala")
+    storage_condition_english = fields.Text(string="Storage Condition (English)",
+                                            related="product_tmpl_id.storage_condition_english")
+    storage_condition_sinhala = fields.Text(string="Storage Condition (Sinhala)",
+                                            related="product_tmpl_id.storage_condition_sinhala")
+    hazard_identification_english = fields.Text(string="Hazard Identification (English)",
+                                                related="product_tmpl_id.hazard_identification_english")
+    hazard_identification_sinhala = fields.Text(string="Hazard Identification (Sinhala)",
+                                                related="product_tmpl_id.hazard_identification_sinhala")
+    health = fields.Selection(
+        [('not_available', 'N/A'), ('health0', '0'), ('health1', '1'), ('health2', '2'), ('health3', '3'),
+         ('health4', '4'), ('health5', '5')], string="Health", related="product_tmpl_id.health")
+    flammability = fields.Selection(
+        [('not_available', 'N/A'), ('flammability0', '0'), ('flammability1', '1'), ('flammability2', '2'),
+         ('flammability3', '3'), ('flammability4', '4'), ('flammability5', '5')], string="Flammability",
+        related="product_tmpl_id.flammability")
+    reactivity_level = fields.Selection(
+        [('not_available', 'N/A'), ('reactivity0', '0'), ('reactivity1', '1'), ('reactivity2', '2'),
+         ('reactivity3', '3'), ('reactivity4', '4'), ('reactivity5', '5')], string="Reactivity",
+        related="product_tmpl_id.reactivity_level")
+    contact = fields.Selection(
+        [('not_available', 'N/A'), ('contact0', '0'), ('contact1', '1'), ('contact2', '2'),
+         ('contact3', '3'), ('contact4', '4'), ('contact5', '5')], string="Contact",
+        related="product_tmpl_id.contact")
+    severity = fields.Selection(
+        [('1', '1'), ('2', '2'),
+         ('3', '3'), ('4', '4'), ('5', '5')], string="Severity", related="product_tmpl_id.severity")
+    likelihood = fields.Selection(
+        [('1', '1'), ('2', '2'),
+         ('3', '3'), ('4', '4'), ('5', '5')], string="Likelihood", related="product_tmpl_id.likelihood")
+    risk_rating = fields.Integer(string="Risk Rating", related="product_tmpl_id.risk_rating")
+    storage_class = fields.Many2one(comodel_name="storage.class", string="Storage Class",
+                                    related="product_tmpl_id.storage_class")
+    water = fields.Boolean(string="Water", related="product_tmpl_id.water")
+    foom = fields.Boolean(string="Foom", related="product_tmpl_id.foom")
+    dry_powder = fields.Boolean(string="Dry Powder", related="product_tmpl_id.dry_powder")
+    carbon_dioxide = fields.Boolean(string="Carbon Dioxide", related="product_tmpl_id.carbon_dioxide")
+    inhalation_english = fields.Text(string="Inhalation (English)", related="product_tmpl_id.inhalation_english")
+    inhalation_sinhala = fields.Text(string="Inhalation (Sinhala)", related="product_tmpl_id.inhalation_sinhala")
+    eye_contact_english = fields.Text(string="Eye Contact (English)", related="product_tmpl_id.eye_contact_english")
+    eye_contact_sinhala = fields.Text(string="Eye Contact (Sinhala)", related="product_tmpl_id.eye_contact_sinhala")
+    skin_contact_english = fields.Text(string="Skin Contact (English)", related="product_tmpl_id.skin_contact_english")
+    skin_contact_sinhala = fields.Text(string="Skin Contact (Sinhala)", related="product_tmpl_id.skin_contact_sinhala")
+    ingestion_english = fields.Text(string="Ingestion (English)", related="product_tmpl_id.ingestion_english")
+    ingestion_sinhala = fields.Text(string="Ingestion (Sinhala)", related="product_tmpl_id.ingestion_sinhala")
+    general_english = fields.Text(string="General (English)", related="product_tmpl_id.general_english")
+    general_sinhala = fields.Text(string="General (Sinhala)", related="product_tmpl_id.general_sinhala")
+    appearance = fields.Many2one(comodel_name="appearance", string="Appearance",
+                                 related="product_tmpl_id.appearance")
+    chemical_color = fields.Many2one(comodel_name="chemical.color", string="Color",
+                                     related="product_tmpl_id.chemical_color")
+    odor = fields.Many2one(comodel_name="odor", string="Odor", related="product_tmpl_id.odor")
+    stability = fields.Many2one(comodel_name="stability", string="Stability", related="product_tmpl_id.stability")
+    incompatible_materials = fields.Many2many(comodel_name="incompatible.materials", string="Incompatible Materials",
+                                              related="product_tmpl_id.incompatible_materials")
+    reactivity = fields.Many2one(comodel_name="reactivity", string="Reactivity",
+                                 related="product_tmpl_id.reactivity")
 
     @api.model
     def create(self, values):
@@ -323,7 +418,7 @@ class OtherName(models.Model):
 class ChemicalFormulatorType(models.Model):
     _name = "chemical.formulator.type"
 
-    name = fields.Text(string="Chemical Formulator type")
+    name = fields.Text(string="Chemical Formulator Type")
 
 
 class Formulator(models.Model):
